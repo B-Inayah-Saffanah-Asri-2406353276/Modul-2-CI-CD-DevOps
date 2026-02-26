400566550a6c6c64d29ad1a2955344fe8bba1e42
Inaeah <nayahsaffanah@gmail.com>
2026-02-26 16:42:24 +0700

    refactor add repository interfaces

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryInterface.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+import id.ac.ui.cs.advprog.eshop.model.Product;
+
+import java.util.Iterator;
+
+public interface ProductRepositoryInterface {
+    Product create(Product product);
+    Product edit(Product updatedProduct);
+    Product findById(String productId);
+    Iterator<Product> findAll();
+    void delete (String productId);
+}
